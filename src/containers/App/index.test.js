1723c32b57204b86e30524e1ac15b1b5f0fa1d29
Tobias Winkler <info@tobiaswinkler.berlin>
2019-07-26 12:22:47 +0200

    tests for App container component
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

---
+++ src/containers/App/index.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import renderer from 'react-test-renderer';
+
+import App from './';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const defaultProps = {
+  goBack: () => {},
+  goNext: () => {},
+};
+
+function setup() {
+  const enzymeWrapper = mount(<App />);
+
+  return { enzymeWrapper };
+}
+
+describe('App', () => {
+  it('should render self and subcomponents', () => {
+    const { enzymeWrapper, props } = setup();
+
+    expect(enzymeWrapper.find('FriendListApp').exists()).toBe(true);
+  });
+
+  it('should match the snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
